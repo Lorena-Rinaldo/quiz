ad51d0693e41dfeb12193f5ab308a6dc90c98e43
Lorena-Rinaldo <lorena_edu@outlook.com>
2026-08-04 14:12:24 -0300

    Feat/Style: Inserção de lógica para passar as perguntas e informar o seu estado(correto/errado)

--- components/QuizScreen.tsx
+++ components/QuizScreen.tsx
@@ -1,26 +1,86 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import questions from "../questions.json";
 
 export default function QuizScreen() {
+  const [indiceAtual, setIndiceAtual] = useState(0);
+  const [pontuacao, setPontuacao] = useState(0);
+  const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
+  const [quizFinalizado, setQuizFinalizado] = useState(false);
+
+  const currentQuestion = questions[indiceAtual];
+
+  function alterarQuestao(opcao) {
+    if (opcaoSelecionada !== null) return;
+
+    setOpcaoSelecionada(opcao);
+
+    if (opcao === currentQuestion.correctAnswer) {
+      setPontuacao(pontuacao + 1);
+    }
+
+    setTimeout(() => {
+      const proximoIndice = indiceAtual + 1;
+
+      if (proximoIndice < questions.length) {
+        setIndiceAtual(proximoIndice);
+        setOpcaoSelecionada(null);
+      } else {
+        setQuizFinalizado(true);
+      }
+    }, 2500);
+  }
+
+  if (quizFinalizado) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.questionText}>Quiz Finalizado!</Text>
+        <Text style={styles.optionText}>
+          Sua pontuação: {pontuacao} de {questions.length}
+        </Text>
+        <TouchableOpacity
+          style={[styles.option, { marginTop: 20 }]}
+          onPress={() => {
+            setIndiceAtual(0);
+            setPontuacao(0);
+            setQuizFinalizado(false);
+            setOpcaoSelecionada(null);
+          }}
+        >
+          <Text>Recomeçar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.questionContainer}>
-        <Text style={styles.questionText}>Qual propriedade do Flexbox é usada para distribuir os itens ao longo
-          do eixo principal?</Text>
+        <Text style={styles.questionText}>{currentQuestion.question}</Text>
       </View>
-      <View>
-        <TouchableOpacity style={styles.option}>
-          <Text style={styles.optionText}>A) alignItems</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.option}>
-          <Text style={styles.optionText}>B) justifyContent</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.option}>
-          <Text style={styles.optionText}>C) flexDirection</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.option}>
-          <Text style={styles.optionText}>D) flexWrap</Text>
-        </TouchableOpacity>
+
+      <View style={styles.optionsContainer}>
+        {currentQuestion.options.map((option) => {
+          let estiloBotao = styles.option;
+          if (opcaoSelecionada !== null) {
+            if (option === currentQuestion.correctAnswer){
+              estiloBotao = styles.correctOption;
+            }
+            else if(option === opcaoSelecionada){
+              estiloBotao = styles.wrongOption;
+            }
+          }
+
+          return (
+            <TouchableOpacity
+              key={option}
+              style={estiloBotao}
+              onPress={() => alterarQuestao(option)}
+            >
+              <Text style={styles.optionText}>{option}</Text>
+            </TouchableOpacity>
+          );
+        })}
       </View>
     </View>
   );
@@ -29,8 +89,8 @@ export default function QuizScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: "#fff",
+    padding: 32,
+    backgroundColor: "#EFDFBB",
   },
   questionContainer: {
     flex: 1,
@@ -38,12 +98,13 @@ const styles = StyleSheet.create({
     padding: 16,
     marginBottom: 20,
     borderRadius: 12,
-    backgroundColor: "white",
+    backgroundColor: "#D2A56C",
   },
   questionText: {
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
+    color: "#fff",
   },
   optionsContainer: {
     flex: 1,
@@ -53,8 +114,22 @@ const styles = StyleSheet.create({
     padding: 16,
     borderRadius: 12,
     borderWidth: 2,
-    borderColor: "#e0e0e0",
-    backgroundColor: "fff",
+    borderColor: "#C8934F",
+    backgroundColor: "#fff",
+  },
+  correctOption: {
+    padding: 16,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: "#63783F",
+    backgroundColor: "#99CE85",
+  },
+  wrongOption: {
+    padding: 16,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: "#A70100",
+    backgroundColor: "#FF5455",
   },
   optionText: {
     fontSize: 18,
