e79d6c8be1c7277250284942ac343e2e985e15cc
Lorena-Rinaldo <lorena_edu@outlook.com>
2026-08-18 14:14:12 -0300

    Feat: Lógica de mudança de telas

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,54 @@
-import QuizScreen from "@/components/QuizScreen";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
 
-const HomePage = () => {
-  return <QuizScreen/>;
-};
+import StartScreen from "../components/StartScreen";
+import QuizScreen from "../components/QuizScreen";
+import ResultScreen from "../components/ResultScreen";
+import questions from "../questions.json";
 
-export default HomePage;
+export default function HomePage() {
+
+  const [currentScreen, setCurrentScreen] = useState("START");
+  const [finalScore, setFinalScore] = useState(0);
+
+  const handleStartGame = () => {
+    setCurrentScreen("QUIZ");
+  };
+
+  const handleFinishQuiz = (score) => {
+    setFinalScore(score);
+    setCurrentScreen("RESULT");
+  };
+
+  const handleRestart = () => {
+    setFinalScore(0);
+    setCurrentScreen("QUIZ");
+  };
+
+  return (
+    <View style={styles.container}>
+      {currentScreen === "START" && (
+        <StartScreen onStart={handleStartGame} />
+      )}
+
+      {currentScreen === "QUIZ" && (
+        <QuizScreen onFinish={handleFinishQuiz} />
+      )}
+
+      {currentScreen === "RESULT" && (
+        <ResultScreen 
+          score={finalScore} 
+          totalQuestions={questions.length} 
+          onRestart={handleRestart} 
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EFDFBB",
+  },
+});
