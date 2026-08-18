1a989c2b6464b62dc22477865aad616d5bfa4b5a
Lorena-Rinaldo <lorena_edu@outlook.com>
2026-08-18 13:56:28 -0300

    Feat: Adição de header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#D2A56C",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 22,
+        },
+        headerTitleAlign: "center",
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Quiz Bíblico",
+        }}
+      />
+    </Stack>
+  );
 }
